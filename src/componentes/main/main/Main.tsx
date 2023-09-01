27039ca9d3618a2053c63eefbe7a50c2d6634d03
LHBR <lucas.rocha5@estudante.ifms.edu.br>
2023-09-01 08:11:41 -0400

    VD

--- src/componentes/main/main/Main.tsx
+++ src/componentes/main/main/Main.tsx
@@ -2,7 +2,7 @@ import Filme from "../../filme/Filme"
 import './Main.css'
 export default function Main(){
     return(
-        <main>
+        <main className="content_main">
             <Filme titulo="Barbie" sinopse="Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita,
              Barbie parte para o mundo humano em busca da verdadeira felicidade."
              imagem="/barbie.png"
