bc4c2ecbe64850195bf30b3c99aade1a5a86fb1a
LHBR <lucas.rocha5@estudante.ifms.edu.br>
2023-11-23 08:37:48 -0400

    tere1

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,37 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Filme from "../filme/Filme";
 import './Main.css'
-type FilmesType = {
-    id: number,
+import axios from "axios";
+// type FilmesType = {
+//     id: number,
+//     titulo:string,
+//     sinopse:string,
+//     imagem:string
+// }
+type FilmeDTO = {
+    id:number, 
     titulo:string,
-    sinopse:string,
+    descricao:string,
     imagem:string
 }
 export default function Main(){
     //Hook
+    const [filmes, setFilme] = useState<FilmeDTO[]>([])
+    useEffect(() => {
+        axios.get(`${"http://localhost:3000/filmes"}`)
+        .then((banco) => {
+            setFilme(banco.data)
+        })
+    })
     const [texto,setTexto] = useState("")
 
-    const filmes:FilmesType[] = [
-        {
-            id:1,
-            titulo:"Barbie",
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:"/barbie.png"
-        },
-        {
-            id:2,
-            titulo:"Transformers",
-            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            imagem:"/transformers.jpg"
-        },
-        {
-            id:3,
-            titulo:"A Freira 2",
-            sinopse:"Em 1956, na França, um padre é assassinado e parece que um mal está se espalhando. Determinada a deter o maligno, irmã Irene mais uma vez fica cara a cara com uma força demoníaca.",
-            imagem:"/afreira2.png"
-        }
+    // const filmes:FilmesType[] = [
+    //     {
+    //         id:1,
+    //         titulo:"Barbie",
+    //         sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+    //         imagem:"/barbie.png"
+    //     },
+    //     {
+    //         id:2,
+    //         titulo:"Transformers",
+    //         sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+    //         imagem:"/transformers.jpg"
+    //     },
+    //     {
+    //         id:3,
+    //         titulo:"A Freira 2",
+    //         sinopse:"Em 1956, na França, um padre é assassinado e parece que um mal está se espalhando. Determinada a deter o maligno, irmã Irene mais uma vez fica cara a cara com uma força demoníaca.",
+    //         imagem:"/afreira2.png"
+    //     }
 
-    ]
+    //]
     //A função recebe um atributo chamado e de "event"
     function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
         console.log(e.target.value)
@@ -50,10 +64,10 @@ export default function Main(){
             <main className="content-main">
                 {filmes
                 .filter((filme)=>filme.titulo.toLowerCase().includes(texto.toLowerCase()))
-                .map((filme:FilmesType)=>
+                .map((filme:FilmeDTO)=>
                     <Filme key={filme.id} 
                            titulo={filme.titulo} 
-                           sinopse={filme.sinopse} 
+                           descricao={filme.descricao} 
                            imagem={filme.imagem}
                     />
                     )
